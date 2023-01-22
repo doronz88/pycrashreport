e719100ae2562dcaa7684f30e2658aed6ddeef4e
dev <yanivhasbanidev@gmail.com>
2023-01-22 19:32:34 +0200

    kernel_panic: add support(la_panic required and parse_crash)

--- pycrashreport/__main__.py
+++ pycrashreport/__main__.py
@@ -1,12 +1,29 @@
 import click
+import json
+from pathlib import Path
 
+from la_panic.panic_parser.kernel_panic import KernelPanic
 from pycrashreport.crash_report import CrashReport
+
+
+def parse_crash(crash_report_file: click.File) -> object:
+    __KERNEL_PANIC_BUG_TYPES = [10, 110, 210]
+
+    with Path(crash_report_file.name).open("rb") as crash_report_filename:
+        raw_metadata = crash_report_filename.readline()
+        metadata = json.loads(raw_metadata)
+
+        bug_type = int(metadata["bug_type"])
+        if bug_type in __KERNEL_PANIC_BUG_TYPES:
+            return KernelPanic(crash_report_file.read(), crash_report_file.name)
+
+        return CrashReport(crash_report_file.read(), crash_report_file.name)
 
 
 @click.command()
 @click.argument('file', type=click.File('rt'))
 def cli(file):
-    print(CrashReport(file.read(), filename=file.name))
+    print(parse_crash(file))
 
 
 if __name__ == '__main__':
