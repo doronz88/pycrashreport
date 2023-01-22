201944054e4cbda1f5fa908b17058025947495d2
dev <yanivhasbanidev@gmail.com>
2023-01-22 18:14:00 +0200

    kernel_panic: added la_panic + kernel panic parsing support

--- pycrashreport/__main__.py
+++ pycrashreport/__main__.py
@@ -1,12 +1,17 @@
 import click
+from la_panic.panic_parser.kernel_panic import KernelPanic
 
 from pycrashreport.crash_report import CrashReport
 
 
 @click.command()
 @click.argument('file', type=click.File('rt'))
-def cli(file):
-    print(CrashReport(file.read(), filename=file.name))
+@click.option('-p', '--panic', is_flag=True, default=False, help="set for kernel panic")
+def cli(file, panic):
+    if panic:
+        print(KernelPanic(file.read(), filename=file.name))
+    else:
+        print(CrashReport(file.read(), filename=file.name))
 
 
 if __name__ == '__main__':
