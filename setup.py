2d0dae399b34d37a8c0a23146f5cea5275cd94ad
DoronZ <doron88@gmail.com>
2023-01-26 00:35:00 +0200

    bump version to 1.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import os
 
 BASE_DIR = os.path.realpath(os.path.dirname(__file__))
-VERSION = '1.0.3'
+VERSION = '1.0.4'
 PACKAGE_NAME = 'pycrashreport'
 PACKAGES = [p for p in find_packages() if not p.startswith('tests')]
 
